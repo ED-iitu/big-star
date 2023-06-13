9514bd085d2ddb9cfd187c4cd32521fb0b0bdfd0
pelivan <pelivan@kolesa.kz>
2023-06-14 01:24:35 +0500

    cloudpayments integration and localization

--- app/Pocket.php
+++ app/Pocket.php
@@ -2,9 +2,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Traits\Translatable;
 
 class Pocket extends Model
 {
+    use Translatable;
+
+    protected $translatable = ['title', 'description'];
+
     protected $fillable = [
         'title',
         'description',
